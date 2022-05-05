a13f25689617d32285c26cdc3a0232a2773afd23
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-05-05 14:29:54 +0200

    Create & Edit

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -32,20 +32,40 @@ sap.ui.define([
 			
 			oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
 			this._bTechnicalErrors = false;
-			
-			// var oJSONData = {
-			// 	busy : false,
-			// 	order:0
-			// };
-			// var oModel = new JSONModel(oJSONData);
-			// this.getView().setModel(oModel, "appView");
 		},
+		
+		onCreate : function () {
+			var oList = this.byId("peopleList"),
+				oBinding = oList.getBinding("items"),
+				oContext = oBinding.create({
+					"UserName" : "",
+					"FirstName" : "",
+					"LastName" : "",
+					"Age" : "18"
+				});
 
+			this._setUIChanges();
+			this.getView().getModel("appView").setProperty("/usernameEmpty", true);
 
-		/* =========================================================== */
-		/*           begin: event handlers                             */
-		/* =========================================================== */
-
+			oList.getItems().some(function (oItem) {
+				if (oItem.getBindingContext() === oContext) {
+					oItem.focus();
+					oItem.setSelected(true);
+					return true;
+				}
+			});
+		},
+		
+		onInputChange : function (oEvt) {
+			if (oEvt.getParameter("escPressed")) {
+				this._setUIChanges();
+			} else {
+				this._setUIChanges(true);
+				if (oEvt.getSource().getParent().getBindingContext().getProperty("UserName")) {
+					this.getView().getModel("appView").setProperty("/usernameEmpty", false);
+				}
+			}
+		},
 
 		/**
 		 * Refresh the data.
@@ -59,6 +79,12 @@ sap.ui.define([
 			}
 			oBinding.refresh();
 			MessageToast.show(this._getText("refreshSuccessMessage"));
+		},
+		
+		onResetChanges : function () {
+			this.byId("peopleList").getBinding("items").resetChanges();
+			this._bTechnicalErrors = false; 
+			this._setUIChanges();
 		},
 		
 		onSearch : function () {
@@ -85,6 +111,33 @@ sap.ui.define([
 			sMessage = this._getText("sortMessage", [this._getText(aStateTextIds[iOrder])]);
 			MessageToast.show(sMessage);
 			},
+			
+			onMessageBindingChange : function (oEvent) {
+			var aContexts = oEvent.getSource().getContexts(),
+				aMessages,
+				bMessageOpen = false;
+
+			if (bMessageOpen || !aContexts.length) {
+				return;
+			}
+
+			// Extract and remove the technical messages
+			aMessages = aContexts.map(function (oContext) {
+				return oContext.getObject();
+			});
+			sap.ui.getCore().getMessageManager().removeMessages(aMessages);
+
+			this._setUIChanges(true);
+			this._bTechnicalErrors = true;
+			MessageBox.error(aMessages[0].message, {
+				id : "serviceErrorMessageBox",
+				onClose : function () {
+					bMessageOpen = false;
+				}
+			});
+
+			bMessageOpen = true;
+		},
 
 
 		/* =========================================================== */
@@ -102,13 +155,20 @@ sap.ui.define([
 			return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 		},
 		
-		_setUIChanges : function(bHasUIChanges){
+		_setUIChanges : function (bHasUIChanges) {
 			if (this._bTechnicalErrors) {
 				// If there is currently a technical error, then force 'true'.
 				bHasUIChanges = true;
 			} else if (bHasUIChanges === undefined) {
-				bHasUIChanges = this.this.getView().getModel().hasPendingChanges();          
+				bHasUIChanges = this.getView().getModel().hasPendingChanges();
 			}
-		}
+			var oModel = this.getView().getModel("appView");
+			oModel.setProperty("/hasUIChanges", bHasUIChanges);
+		},
+		
+		_setBusy : function(){
+			var oModel = this.getView().getModel("appView");
+				oModel.setProperty("/busy", bIsBusy);
+		},
 	});
 });
