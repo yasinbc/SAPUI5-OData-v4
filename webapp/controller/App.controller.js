16721350508362e861abe46017622b5b306efd51
Yasin Boulahya Chairi <YBC5291@cemosa.es>
2022-05-06 09:23:21 +0200

    OData Operations

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -99,6 +99,19 @@ sap.ui.define([
 			this._setUIChanges();
 		},
 		
+		onResetDataSource : function () {
+			var oModel = this.getView().getModel(),
+				oOperation = oModel.bindContext("/ResetDataSource(...)");
+
+			oOperation.execute().then(function () {
+					oModel.refresh();
+					MessageToast.show(this._getText("sourceResetSuccessMessage"));
+				}.bind(this), function (oError) {
+					MessageBox.error(oError.message);
+				}
+			);
+		},
+		
 		onSearch : function () {
 			var oView = this.getView(),
 				sValue = oView.byId("searchField").getValue(),
